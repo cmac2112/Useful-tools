a570ba21dbb37dcf3c5c894ac090a19c230e117e
Caden McArthur <cadenamcarthur@bethelks.edu>
2024-02-02 08:47:02 -0600

    youtube downloader

---
+++ yt_downloader.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+
+def download(url, path):
+    #get input from user on url
+    try:
+        yt = YouTube(url) #create youtube object
+
+        stream = yt.streams.get_highest_resolution() 
+
+        stream.download(output_path=path) #begin download with savepath = userpath from before
+        print("Downloaded sucess\n")
+    except Exception as e:
+        print("Error occurred somewhere:", str(e))
+
+if __name__ == "__main__":
+    user_url = input("Enter Valid Url From Youtube: ")
+    print("If you wish to download the video to the downloads folder, enter 1")
+    user_path = input("Enter a path for the video to be downloaded to: ")
+
+    if user_path == '1':
+        user_path = 'C://Users//ousoo//downloads'
+    download(user_url, user_path)
